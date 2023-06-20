e3a6491f7c2e8873725e12d21c67dbf3e41cadb8
s22203-pjatk <s22203@pjwstk.edu.pl>
2023-06-20 18:33:11 +0200

    Create customhook

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,10 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
-          aside
-        </GridItem>
+        <GridItem area="aside">aside</GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid/>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
