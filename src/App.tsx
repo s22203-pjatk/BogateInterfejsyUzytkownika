bc874876226567dde3298e2d41148deac770260b
s22203-pjatk <s22203@pjwstk.edu.pl>
2023-06-20 17:42:10 +0200

    build navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-      
-    }}>
-      <GridItem area="nav" bg="coral">
-        nav
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar/>
       </GridItem>
       <Show above="lg">
-      <GridItem area="aside" bg="gold">
-        aside
-      </GridItem>
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
       </Show>
       <GridItem area="main" bg="dodgerblue">
         main
