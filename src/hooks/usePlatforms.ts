407ca568fc062bdd7fb0f6f5bfcad4fa4fc9660c
s22203-pjatk <s22203@pjwstk.edu.pl>
2023-06-21 19:29:01 +0200

    Fetching Games using Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
 
 
@@ -13,7 +13,7 @@ interface Platform {
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: () => apiClient
-        .get<FetchResponse<Platform>>('/platforms')
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
         .then(res => res.data),
     staleTime: 24*60*60*1000, //24h
     initialData: {count:platforms.length, results:platforms}
